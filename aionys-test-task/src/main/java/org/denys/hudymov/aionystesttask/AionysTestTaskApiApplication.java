dfcc4c393f52b8aecb5eaa86e67d6f74b8b7200e
Denys <denys.hudymov@gmail.com>
2024-05-13 00:24:11 +0300

    It is the main entry point to the program

---
+++ aionys-test-task/src/main/java/org/denys/hudymov/aionystesttask/AionysTestTaskApiApplication.java
@@ -0,0 +1,13 @@
+package org.denys.hudymov.aionystesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AionysTestTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AionysTestTaskApiApplication.class, args);
+    }
+
+}
