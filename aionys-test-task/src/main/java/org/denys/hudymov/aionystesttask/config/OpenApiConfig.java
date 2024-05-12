25b2c66f91bbe9743670b3c64259ccb36e51e386
Denys <denys.hudymov@gmail.com>
2024-05-13 00:19:36 +0300

    I created a configuration class for the OpenApi documentation

---
+++ aionys-test-task/src/main/java/org/denys/hudymov/aionystesttask/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package org.denys.hudymov.aionystesttask.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Denys",
+                        email = "denys.hudymov@gmail.com"
+                ),
+                description = "OpenAPI documentation for the Test task",
+                title = "OpenAPI specification - Denys",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+public class OpenApiConfig {
+}
